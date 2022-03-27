63bed43f0f6534d82c6dd8b920c2fbd92166a1ec
kentaro minami <minami.kentaro123@gmail.com>
2022-03-27 12:19:42 +0900

    add model

---
+++ src/model.cpp
@@ -0,0 +1,48 @@
+﻿#include "model.h"
+
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#include "util.h"
+
+Model::Model()
+{}
+
+Model::~Model()
+{}
+
+bool Model::Load(const char* filename)
+{
+    std::ifstream ifs{filename};
+    if (!ifs.is_open()) {
+        return false;
+    }
+
+    std::string line;
+    while (!ifs.eof()) {
+        std::getline(ifs, line);
+        std::istringstream iss(line);
+        char trash;
+        if (line.compare(0, 2, "v ") == 0) {
+            iss >> trash;
+            Vec3 v;
+            iss >> v.x;
+            iss >> v.y;
+            iss >> v.z;
+            m_verts.push_back(v);
+
+        } else if (line.compare(0, 2, "f ") == 0) {
+            std::vector<uint32_t> f;
+            int32_t itrash, index;
+            iss >> trash;
+            while (iss >> index >> trash >> itrash >> trash >> itrash) {
+                --index;
+                f.push_back(index);
+            }
+            m_faces.push_back(std::move(f));
+        }
+    }
+    INFOF("# v# %zu f# %zu", m_verts.size(), m_faces.size());
+    return true;
+}
---
+++ src/model.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include <vector>
+
+#include "vec3.h"
+
+class Model final
+{
+public:
+    Model();
+    ~Model();
+
+    bool Load(const char* filename);
+    uint32_t GetNumVerts() const { return static_cast<uint32_t>(m_verts.size()); }
+    uint32_t GetNumFaces() const { return static_cast<uint32_t>(m_faces.size()); }
+    const Vec3& GetVert(uint32_t i) const { return m_verts[i]; }
+    const std::vector<uint32_t>& GetFace(uint32_t i) const { return m_faces[i]; }
+
+private:
+    std::vector<Vec3> m_verts{};
+    std::vector<std::vector<uint32_t>> m_faces{};
+};
