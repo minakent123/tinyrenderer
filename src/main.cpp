72fe840dbd51e8080408d864f70bf621e1c4912e
kentaro minami <minami.kentaro123@gmail.com>
2022-04-03 17:55:28 +0900

    draw model with random colors

--- src/main.cpp
+++ src/main.cpp
@@ -5,49 +5,102 @@
 #include "tga.h"
 #include "util.h"
 #include "vec2.h"
-#include "vec3.h"
 
-Vec3f Barycentric(Vec2i* pts, Vec2i p)
+void DrawLine(int32_t x0, int32_t y0, int32_t x1, int32_t y1, TGAImage& image, const TGAColor& color)
 {
-    Vec3f u = Vec3f(static_cast<float>(pts[2].x - pts[0].x), static_cast<float>(pts[1].x - pts[0].x), static_cast<float>(pts[0].x - p.x)).Cross(Vec3f(static_cast<float>(pts[2].y - pts[0].y), static_cast<float>(pts[1].y - pts[0].y), static_cast<float>(pts[0].y - p.y)));
-    if (std::abs(u.z) < 1) {
-        return Vec3f(-1, 1, 1);
+    bool steep = false;
+    if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
     }
-    return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
+    if (x0 > x1) {
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+    int32_t dx = x1 - x0;
+    int32_t dy = y1 - y0;
+    int32_t derror = std::abs(dy) * 2;
+    int32_t error = 0;
+    int32_t y = y0;
+    for (int32_t x = x0; x <= x1; ++x) {
+        if (steep) {
+            image.SetColor(y, x, color);
+        } else {
+            image.SetColor(x, y, color);
+        }
+        error += derror;
+        if (error > dx) {
+            y += (y0 < y1 ? 1 : -1);
+            error -= dx * 2;
+        }
+    }
 }
 
-void DrawTraiangle(Vec2i* pts, TGAImage& image, const TGAColor& color)
+void DrawTraiangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, const TGAColor& color)
 {
-    Vec2i bboxmin(image.GetWidth() - 1, image.GetHeight() - 1);
-    Vec2i bboxmax(0, 0);
-    Vec2i clamp(image.GetWidth() - 1, image.GetHeight() - 1);
-
-    for (int32_t i = 0; i < 3; ++i) {
-        bboxmin.x = std::max(0, std::min(bboxmin.x, pts[i].x));
-        bboxmin.y = std::max(0, std::min(bboxmin.y, pts[i].y));
-        bboxmax.x = std::min(clamp.x, std::max(bboxmax.x, pts[i].x));
-        bboxmax.y = std::min(clamp.y, std::max(bboxmax.y, pts[i].y));
+    if (t0.y == t1.y && t0.y == t2.y) {
+        return;
     }
-
-    Vec2i p;
-    for (p.x = bboxmin.x; p.x <= bboxmax.x; ++p.x) {
-        for (p.y = bboxmin.y; p.y <= bboxmax.y; ++p.y) {
-            Vec3f bc_screen = Barycentric(pts, p);
-            if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) {
-                continue;
-            }
-            image.SetColor(p.x, p.y, color);
+    if (t0.y > t1.y) {
+        std::swap(t0, t1);
+    }
+    if (t0.y > t2.y) {
+        std::swap(t0, t2);
+    }
+    if (t1.y > t2.y) {
+        std::swap(t1, t2);
+    }
+    int32_t total_height = t2.y - t0.y;
+    for (int32_t i = 0; i < total_height; ++i) {
+        float alpha = static_cast<float>(i) / total_height;
+        Vec2i a = t0 + (t2 - t0) * alpha;
+        Vec2i b;
+        bool second_half = (i > t1.y - t0.y) || (t1.y == t0.y);
+        if (second_half) {
+            int32_t segment_height = t2.y - t1.y;
+            float beta = static_cast<float>(i - (t1.y - t0.y)) / segment_height;
+            b = t1 + (t2 - t1) * beta;
+        } else {
+            int32_t segment_height = t1.y - t0.y;
+            float beta = static_cast<float>(i) / segment_height;
+            b = t0 + (t1 - t0) * beta;
+        }
+        if (a.x > b.x) {
+            std::swap(a, b);
+        }
+        for (int32_t j = a.x; j < b.x; ++j) {
+            image.SetColor(j, t0.y + i, color);
         }
     }
 }
 
 int main(int argc, char** argv)
 {
-    TGAColor red = TGAColor(255, 0, 0, 255);
-    TGAImage image(200, 200, TGAFormat::RGB);
-    Vec2i pts[3] = {Vec2i(10, 10), Vec2i(100, 30), Vec2i(190, 160)};
-    DrawTraiangle(pts, image, red);
+    if (argc != 2) {
+        ERRORF("usage: tinyrenderer path_to_obj");
+        return -1;
+    }
+    Model* model = new Model();
+    if (!model->Load(argv[1])) {
+        ERRORF("failed to load %s", argv[1]);
+        delete model;
+        return -1;
+    }
+    const int32_t width = 800;
+    const int32_t height = 800;
+    TGAImage image(width, height, TGAFormat::RGB);
+    for (uint32_t i = 0; i < model->GetNumFaces(); ++i) {
+        const std::vector<uint32_t>& face = model->GetFace(i);
+        Vec2i screenCoords[3];
+        for (uint32_t j = 0; j < 3; ++j) {
+            const Vec3f& worldCoords = model->GetVert(face[j]);
+            screenCoords[j] = Vec2i(static_cast<int32_t>((worldCoords.x + 1) * width / 2), static_cast<int32_t>((worldCoords.y + 1) * height / 2));
+        }
+        DrawTraiangle(screenCoords[0], screenCoords[1], screenCoords[2], image, TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
+    }
     image.FlipVertically();
     image.Write("output.tga");
+    delete model;
     return 0;
 }
